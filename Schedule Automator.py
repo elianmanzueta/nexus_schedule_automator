0acba66381af7b05ed4e35fb6399adbd81b08aad
Elian Manzueta <37516550+elianmanzueta@users.noreply.github.com>
2022-06-07 12:39:08 -0400

    Formatting changes.

--- Schedule Automator.py
+++ Schedule Automator.py
@@ -14,28 +14,28 @@ sa = gspread.service_account()
 
 print("Hey Hannah, here are my hours: \n")
           
-# Accessing the sheet
+# Accessing the sheet.
 sh = sa.open("ScheduleTest")
 
 # Main function
 def getSchedule(worksheet):
-    wks = sh.worksheet(worksheet) # Selects desired sheet
-    total = wks.acell('I10').value # Total hours
+    wks = sh.worksheet(worksheet) # Selects the desired sheet.
+    total = wks.acell('I10').value # Total hours.
     print(wks.acell('B2').value, '-', wks.acell('H2').value, '\n') # Pay Period
-    days = (wks.get('B2:H2')) # Batch get of all values in B2:H2
-    hours = (wks.get('B10:H10')) # Batch get of all values in B10:H10
+    days = (wks.get('B2:H2')) # Batch get of all values in B2:H2.
+    hours = (wks.get('B10:H10')) # Batch get of all values in B10:H10.
     
     # wks.get() outputs a nested list, so we have to flatten it and convert it to a hashable data type.
     formatted_days = tuple([item for elem in days for item in elem])
     formatted_hours = tuple([item for elem in hours for item in elem])
     
-    # Using zip() to combine formatted_days and formatted_hours into a dictionary
+    # Using zip() to combine formatted_days and formatted_hours into a dictionary.
     dictionary_values = dict(zip(formatted_days, formatted_hours)) 
     
     schedule = '' 
     for key, value in dictionary_values.items():
         # Checks if there's an entry for the date.
-        if value != '':
+        if value != (''):
             # Formatting the dictionary into a string.
             schedule += key + ': ' + value + '\n' 
     print(schedule)
